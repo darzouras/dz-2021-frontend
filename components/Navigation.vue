d1388c8ba6eaf9f111f18476c0a1e807edf2d53f
Darlene Zouras <dzouras@gmail.com>
2021-08-22 15:38:24 -0500

    update: added default layout

---
+++ components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/blog">Blog</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/now">Now</nuxt-link>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="layout">
+        <Navigation />
+        <Nuxt />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+    font-family: 'Inconsolata', sans-serif;
+}
+</style>
