8dfc39eb34c30eb4f81bd0d0186868a17ed330fd
Darlene Zouras <dzouras@gmail.com>
2021-08-22 14:14:02 -0500

    update: added general scss files

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "@assets/css/main.scss"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -53,6 +54,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/markdownit',
     '@nuxtjs/strapi'
   ],
 
